c60d783ceeb68be74cc8e017f8c907861659305d
SMahalaxmi2025 <makarandamkar1189@gmail.com>
2025-02-17 00:42:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+document.getElementById("customerForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    document.getElementById("thank-you").style.display = "block";
+    setTimeout(() => {
+        document.getElementById("thank-you").style.display = "none";
+    }, 3000);
+});
