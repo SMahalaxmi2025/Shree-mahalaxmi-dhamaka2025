7acbf896d03ec58888b73bc3188814f7da831a35
Makarand Raosaheb Amkar <makarandamkar1189@gmail.com>
2025-02-18 14:50:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById("couponForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    alert("✅ तुमची माहिती यशस्वीरीत्या सबमिट झाली! कृपया WhatsApp वर संपर्क साधा.");
+    alert("आपली माहिती यशस्वीरीत्या सबमिट झाली! कृपया WhatsApp वर संपर्क साधा.");
 });
